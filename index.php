0177f7aff38b887b95fb45837b0da83af4c759a1
root <root@debian-server>
2016-01-14 00:58:40 +0100

    Add some server files

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+
+	<head>
+
+	</head>
+
+	<body>
+	
+		<h1>Data recovery system</h1>
+		
+		<form action="downloader.php" method="post">
+			<p>Enter begin date : <input id="begin" name="begin" value="yyyy-mm-dd"></p>
+			<p>Enter end date : <input id="end" name="end" value="yyyy-mm-dd"></p>
+    		<p><input type="submit" value="OK"></p>
+    	</form>
+
+	</body>
+
+
+
+
+</html>
